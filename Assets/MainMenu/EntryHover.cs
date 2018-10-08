e462f4497252007207cb964b2c448a45b75386c7
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-10-08 20:42:01 +0200

    Add main menu options

---
+++ Assets/MainMenu/EntryHover.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace MainMenu {
+    public class MainMenuEntryHover : MonoBehaviour {
+        public Color HoverColor = Color.yellow;
+
+        private AudioSource _audioHoverSource;
+        private Renderer _renderer = new Renderer();
+        private Color _inactiveColor = Color.gray;
+        private bool _isHovering;
+
+        private void Start () {
+            this._audioHoverSource = this.GetComponent<AudioSource>();
+            this._renderer = this.GetComponent<Renderer>();
+            Cursor.visible = true;
+        }
+
+        private void OnMouseEnter() {
+            // we need to ensure this is not being
+            // called multiple times during an update
+            if (this._isHovering) {
+                return;
+            }
+
+            this._inactiveColor = this._renderer.material.color;
+            this._renderer.material.color = this.HoverColor;
+            this._isHovering = true;
+            this._audioHoverSource.Play();
+        }
+
+        private void OnMouseExit() {
+            if (!this._isHovering) {
+                return;
+            }
+
+            this._renderer.material.color = this._inactiveColor;
+            this._isHovering = false;
+        }
+
+        private void Update () {
+        }
+    }
+}
