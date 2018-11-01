242f3532f42f064e9e71313be1a8b72d46ff95f2
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-11-01 22:04:45 +0100

    Add a game save manager with de/serializer

---
+++ Assets/Scripts/Managers/GameState.cs
@@ -0,0 +1,10 @@
+namespace Managers {
+    /// <summary>
+    /// Manages the state of a game, containing all serializable
+    /// data possible that needs get saved and restored.
+    /// </summary>
+    [System.Serializable]
+    public class GameState {
+
+    }
+}
--- Assets/Scripts/Managers/SaveGameManager.cs
+++ Assets/Scripts/Managers/SaveGameManager.cs
@@ -1,4 +1,5 @@
-﻿using Boo.Lang;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace Managers {
@@ -9,10 +10,26 @@ namespace Managers {
     /// <inheritdoc />
     public class SaveGameManager : MonoBehaviour {
         /// <summary>
+        /// The filename that the save game should have.
+        /// </summary>
+        private const string _SAVE_FILENAME = "save0001.dat";
+
+        /// <summary>
+        /// The local path where the save game should be located.
+        /// The value get populated when the game object awakes.
+        /// </summary>
+        public static string SavePath;
+
+        /// <summary>
         /// True if an instance of this game object was created.
         /// Ensuring its uniqueness.
         /// </summary>
         private static bool _created;
+
+        /// <summary>
+        /// Current loaded game state instance object.
+        /// </summary>
+        private static GameState _currentGame = new GameState();
 
         /// <summary>
         /// If it doesn't exist yet, it flags itself as existing
@@ -24,6 +41,9 @@ namespace Managers {
             if (!_created) {
                 DontDestroyOnLoad(this.gameObject);
                 _created = true;
+
+                SavePath = Path.Combine(
+                    Application.persistentDataPath, "savedGames.gd");
             }
             else {
                 Destroy(this.gameObject);
@@ -31,27 +51,38 @@ namespace Managers {
         }
 
         /// <summary>
-        /// Finds and returns the existing game save manager.
+        /// Return <code>True</code> if a save game is available
+        /// to be loaded from on the hard drive.
         /// </summary>
-        /// <returns>The existing self game object.</returns>
-        public static SaveGameManager GetGameManager() {
-            return FindObjectOfType<SaveGameManager>();
+        /// <returns></returns>
+        public static bool SaveGameExists() {
+            return File.Exists(SavePath);
         }
 
         /// <summary>
-        /// Retrieves the list of game saves and returns thems.
+        /// Save the current game state to a save file game.
         /// </summary>
-        /// <returns>The list of existing named game saves.</returns>
-        public List<int> ListSaves() {
-            // FIXME: implement me (see #9)
-            return new List<int>();
+        public static void Save() {
+            // Retrieve the game's local save path and create a new file.
+            using (var fp = File.Create(SavePath)) {
+                // Serialize the current game.
+                new BinaryFormatter().Serialize(fp, _currentGame);
+            }
         }
 
         /// <summary>
         /// Loads the latest game save.
         /// </summary>
-        public void LoadLatestGame() {
-            // FIXME: implement me (see #9)
+        public static void LoadLatest() {
+            // To reduce nesting, we return as we are nothing to do.
+            if (!SaveGameExists()) {
+                return;
+            }
+
+            // Open, and load the file for deserialization.
+            using (var fp = File.Open(SavePath, FileMode.Open)) {
+                _currentGame = (GameState) new BinaryFormatter().Deserialize(fp);
+            }
         }
     }
 }
