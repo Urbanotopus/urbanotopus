153a66d5af75469754b0f365acc1cf88866e0f7b
NyanKiyoshi <hello@vanille.bid>
2018-11-06 22:00:59 +0100

    Add a sphinx shared folder and update RTD for it

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ language = 'fr'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-html_extra_path = ['../doxygens']
+html_extra_path = ['../public_html']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
---
+++ public_html/README.txt
@@ -0,0 +1,2 @@
+This is a shared folder of static files with sphinx.
+Note: a folder named `doxygen` may also get populated by RTD.
