71f43c20d84b20b4d7c3ae3d1b1c006ec6e1c6af
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-11-07 19:14:29 +0100

    Add code documentations

--- Assets/Office/Scripts/LoadButton.cs
+++ Assets/Office/Scripts/LoadButton.cs
@@ -3,12 +3,26 @@ using Managers;
 using UnityEngine.SceneManagement;
 
 namespace Office.Scripts {
+    /// <inheritdoc />
+    /// <summary>
+    /// The Load button component of the office scene.
+    /// Manages the action of loading a quick save.
+    /// </summary>
     public class LoadButton : HoverableButton {
+        /// <summary>
+        /// The load button of the 'Office' scene.
+        /// Called when this component gets initialized,
+        /// it instructs the base instance to call <see cref="LoadSaveGameAndReload"/>,
+        /// which loads the latest save game and reload the scene (the office).
+        /// </summary>
         protected override void Start() {
             base.Start();
             this.onClick.AddListener(LoadSaveGameAndReload);
         }
 
+        /// <summary>
+        /// Load the latest save game (if any), and reload the scene (the Office).
+        /// </summary>
         private static void LoadSaveGameAndReload() {
             SaveGameManager.LoadLatest();
             SceneManager.LoadScene(InternalScenesManager.Office);
--- Assets/Office/Scripts/QuitButton.cs
+++ Assets/Office/Scripts/QuitButton.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Office.Scripts {
+    /// <inheritdoc />
+    /// <summary>
+    /// The Quit button component of the office scene.
+    /// Manages the action of leaving the scene.
+    /// </summary>
     public class QuitButton : HoverableButton {
+        /// <summary>
+        /// Called on the scene initialization.
+        /// Registers to open a prompt when the user clicks on the button.
+        /// </summary>
         protected override void Start() {
             base.Start();
             this.onClick.AddListener(PromptQuit);
         }
 
+        /// <summary>
+        /// Prompt to quit the scene.
+        /// </summary>
         private static void PromptQuit() {
+            // TODO: we need to put a prompt canvas here
             SceneManager.LoadScene(InternalScenesManager.MainMenu);
         }
     }
