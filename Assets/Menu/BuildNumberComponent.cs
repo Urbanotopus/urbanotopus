9e0c9b1944a6d29eceda18cc217dec8421fb84ef
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-11-07 16:23:34 +0100

    Add build number to the main menu

---
+++ Assets/Menu/BuildNumberComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Menu {
+    [ExecuteInEditMode]
+    public class BuildNumberComponent : MonoBehaviour {
+        public int BuildId;
+
+        private void Start () {
+            var textMesh = this.GetComponent<TextMesh>();
+            if (!textMesh) {
+                return;
+            }
+
+            textMesh.text = string.Format("Build #{0}", this.BuildId);
+        }
+    }
+}
