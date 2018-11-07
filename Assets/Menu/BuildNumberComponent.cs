81a54454de0b512df26f8c601fa9f3877548a84b
NyanKiyoshi <6186720+NyanKiyoshi@users.noreply.github.com>
2018-11-07 16:25:33 +0100

    Add missing comments

--- Assets/Menu/BuildNumberComponent.cs
+++ Assets/Menu/BuildNumberComponent.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 
 namespace Menu {
+    /// <inheritdoc />
+    /// <summary>
+    /// A component that populates a text mesh with
+    /// the build number on the main menu.
+    /// </summary>
     [ExecuteInEditMode]
     public class BuildNumberComponent : MonoBehaviour {
+        /// <summary>
+        /// The latest release ID.
+        /// </summary>
         public int BuildId;
 
+        /// <summary>
+        /// Populates a text mesh component with the build number.
+        /// </summary>
         private void Start () {
             var textMesh = this.GetComponent<TextMesh>();
             if (!textMesh) {
